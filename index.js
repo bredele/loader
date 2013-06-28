26c97d0e2c736c540b4a17d0890a51ea9d85b772
bredele <olivier@petrofeed.com>
2013-06-27 23:36:12 -0600

    test relative path

--- index.js
+++ index.js
@@ -40,8 +40,7 @@ function Loader(path) {
  */
 
 Loader.prototype.use = function(name) {
-  var load = require.relative(this.path);
-  var mod = load(require.resolve(name));
+  var mod = require(name, this.path);
   if(typeof mod === 'function'){
     mod.apply(null, toArray(arguments, 1));
   }
